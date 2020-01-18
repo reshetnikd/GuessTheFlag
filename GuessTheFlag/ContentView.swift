1f6f1c68a9145972d4543bc037438c7833572b97
reshetnikd <dmitry.reshetnik@icloud.com>
2020-01-18 21:40:22 +0200

    Add stacking up buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,57 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Text("1")
-                Text("2")
-                Text("3")
-            }
-            
-            HStack {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            
-            HStack {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
-            
-            Button("Tap me!") {
-                print("Button was tapped")
-            }
-            
-            Button("Show Alert") {
-                self.showingAlert = true
-            }.alert(isPresented: $showingAlert) {
-                Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
-            }
-            
-            Button(action: {
-                print("Edit was tapped")
-            }) {
-                HStack(spacing: 10) {
-                    Image(systemName: "pencil").renderingMode(.original)
-                    Text("Edit")
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of").foregroundColor(.white)
+                    Text(countries[correctAnswer]).foregroundColor(.white)
                 }
-            }
-            
-            LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-            
-            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-            
-            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-            
-            ZStack {
-                Color.red.edgesIgnoringSafeArea(.all)
-                Color(red: 1, green: 0.8, blue: 0).frame(width: 200, height: 200, alignment: .center)
-                Text("Your content")
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        //flag was tapped
+                    }) {
+                        Image(self.countries[number]).renderingMode(.original)
+                    }
+                }
+                
+                Spacer()
             }
         }
     }
